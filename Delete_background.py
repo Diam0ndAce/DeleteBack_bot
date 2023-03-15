94bdfeacd74fbe6b0b0cb46613aba09f7136a71f
Diam0ndAce <56152630+Diam0ndAce@users.noreply.github.com>
2023-03-16 01:27:58 +1000

    Update Delete_background.py

--- Delete_background.py
+++ Delete_background.py
@@ -9,8 +9,7 @@ def Get_mask(img):
         mask = model(cv2.resize(img, image_size)[np.newaxis, ...] / 255)[0]
         
         mask = np.argmax(mask, axis=-1)
-        #mask = np.where(mask==0, 1, 0)   # delete back and argue pixels
-        mask = np.where(mask%2==0, 1, 0)  #  delete only back
+        mask = np.where(mask%2==0, 1, 0) 
         
         mask_shape = (img.shape[1], img.shape[0])
         mask = cv2.resize(mask.astype(np.uint8), mask_shape)
@@ -22,7 +21,6 @@ def Delete_back(img):
         
         img_without_back = img * mask 
         img_without_back =  img_without_back.astype(np.uint8)
-        #cv2.imwrite("test1.jpg", img_without_back)
         return img_without_back
  
         
